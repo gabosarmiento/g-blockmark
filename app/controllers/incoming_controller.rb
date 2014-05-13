8bdbad8c1e87b5760b3e8ec3efe3f0f14e2cbeb7
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-05-12 21:37:56 -0500

    Update incomming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,21 +4,18 @@ class IncomingController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:create]
 
   def create
-    
-    @user = params[:sender]
-    puts "sender user #{@user}"
-    @name = params[:subject]
-    puts "category name #{@name}"
-    @url = params[:bodyPlain]
-    puts "bookmark url #{@url}"
-    @bookmark = Bookmark.new(params.require(:bookmark).permit(:url)) 
-    @bookmark.user = @user
-    @bookmark.category = Category.find_or_create_by!(params.require(:category).permit(:name))
-    if @bookmark.save 
-      puts "saved"
-    else
-      puts "error"
-    end
+
+    puts "sender user #{params[:sender]}"
+    puts "category name #{params[:subject]}"
+    puts "bookmark url #{params[:bodyPlain]}"
+    # @bookmark = Bookmark.new(params.require(:bookmark).permit(:url)) 
+    # @bookmark.user = @user
+    # @bookmark.category = Category.find_or_create_by!(params.require(:category).permit(:name))
+    # if @bookmark.save 
+    #   puts "saved"
+    # else
+    #   puts "error"
+    # end
 
     # Assuming all went well. 
     head 200
